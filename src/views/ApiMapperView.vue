7e464b40740f5c4e8dd7ba38d17aa44907b8110f
José R. Ferreyra <jferreyra.dev@gmail.com>
2025-06-10 23:17:54 -0300

    mejora 2

--- src/views/ApiMapperView.vue
+++ src/views/ApiMapperView.vue
@@ -1,21 +1,24 @@
 <script setup>
-import { useFetchCache } from '../composables/useFetchCache'
+import { useFetchWatch } from '../composables/useFetchWatch'
 import { useEndPoint } from '../composables/useEndPoint'
 import JsonViewer from '../components/JsonViewer.vue'
 
 const { activeEndPoint } = useEndPoint()
 
-const { data, loading, error, reload } = useFetchCache('json-demo', activeEndPoint.value?.boletas + '/sp/list')
+const { data, loading, error } = useFetchWatch(activeEndPoint.value?.sp + '/sp/list')
+
+const {_data, _loading, _error} = useFetchWatch(activeEndPoint.value?.base + '/api/view/list')
 
 </script>
 
 <template>
   <div>
     <div class="flex gap-2 mb-2">
-      <button class="btn btn-sm btn-outline" @click="reload">Recargar</button>
+      <button class="btn btn-sm btn-outline" @click="">Recargar</button>
     </div>
     <div v-if="loading">Cargando...</div>
     <div v-else-if="error">{{ error.message }}</div>
     <JsonViewer v-else :json="data" />
+
   </div>
 </template>
